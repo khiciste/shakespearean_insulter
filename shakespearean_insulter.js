dd8afd85baf672eec28faebfcc29d1315deeb3fb
khiciste <khiciste@penguin>
2023-01-14 13:56:58 -0500

    add script & txt

---
+++ shakespearean_insulter.js
@@ -0,0 +1,37 @@
+// shakespearean insult generator
+// returns a randomized insult from 3 separate lists
+
+// list one
+const adjs = ['artless', 'bawdy', 'beslubbering', 'bootless', 'churlish', 'cockered', 'clouted', 'craven', 'currish', 'dankish', 'dissembling', 'droning', 'errant', 'fawning', 'fobbing', 'fobbing', 'froward', 'frothy', 'gleeking', 'goatish', 'gorbellied', 'impertinent', 'infectious', 'jarring', 'loggerheaded', 'lumpish', 'mammering', 'mangled', 'mewling', 'paunchy', 'pribbling', 'puking', 'puny', 'qualling', 'rank', 'reeky', 'roguish', 'ruttish', 'saucy', 'spleeny', 'spongy', 'surly', 'tottering', 'unmuzzled', 'vain', 'venomed', 'villainous', 'warped', 'wayward', 'weedy', 'yeasty'];
+// list two
+const comp_adjs = ['base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten'];
+// list three
+const nouns = ['apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlet', 'vassal', 'whey-face', 'wagtail'];
+
+// random function (list)
+const rand = (list) => {
+  const r = Math.floor(Math.random() * list.length); 
+  return r;
+}
+
+const insult = () => {
+  // get random word from each list
+  const adj = adjs[rand(adjs)];
+  const comp_adj = comp_adjs[rand(comp_adjs)];
+  const noun = nouns[rand(nouns)];
+
+  // log insult
+  let thou_art = 'Thou art a ';
+  if (adj[0] == 'a' ||
+      adj[0] == 'e' ||
+      adj[0] == 'i' ||
+      adj[0] == 'o' ||
+      adj[0] == 'u' ) {
+        thou_art = 'Thou art an';    
+      }
+      else {
+        thou_art = 'Thou art a';
+      }
+      console.log(`${thou_art} ${adj} ${comp_adj} ${noun}.`);
+}
+insult();
